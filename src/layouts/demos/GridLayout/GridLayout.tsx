a2b6a08d70b0f0156346161cf8664c0f212ca034
zrwusa <zrwusa@gmail.com>
2020-12-20 23:15:43 +0800

    nothing

--- src/layouts/demos/GridLayout/GridLayout.tsx
+++ src/layouts/demos/GridLayout/GridLayout.tsx
@@ -15,13 +15,10 @@ const GridLayout: React.FunctionComponent<IProps> = ({title,children}) => {
                     <Text>Nav bar</Text>
                     <NavLinks />
                 </View>
-
                     <Text>Content</Text>
-
                 {children}
                 {/*<aside className="grid-layout__sidebar grid-layout__panel">Side bar section</aside>*/}
                 {/*<div className="grid-layout__ad grid-layout__panel">Advertising section</div>*/}
-
                     <Text>Footer</Text>
             </View>
     );
